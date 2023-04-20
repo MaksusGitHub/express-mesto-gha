a4b78e6fd9dd4a8201c20030c8413ff8d461294c
Maksim Semenov <maksimuma@list.ru>
2023-04-20 20:21:34 +0300

    feat:добавить ещё роуты

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,16 @@
 const usersRouter = require('express').Router();
-const { createUser, getUserById, getUsers } = require('../controllers/users');
+const {
+  createUser,
+  getUserById,
+  getUsers,
+  updateProfile,
+  updateAvatar,
+} = require('../controllers/users');
 
 usersRouter.get('/', getUsers);
 usersRouter.get('/:id', getUserById);
 usersRouter.post('/', createUser);
+usersRouter.patch('/me', updateProfile);
+usersRouter.patch('/me/avatar', updateAvatar);
 
 module.exports = usersRouter;
