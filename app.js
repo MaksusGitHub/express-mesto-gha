adde4e402c1e044e82c961420dc81858c79aae01
Maksim Semenov <maksimuma@list.ru>
2023-04-26 18:23:12 +0300

    feat: добавить роут для получения инфы о пользователе

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const auth = require('./middlewares/auth');
@@ -13,11 +14,12 @@ const app = express();
 
 mongoose.connect(DB);
 
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.post('/signup', createUser);
 app.post('/signin', login);
-app.post('/singup', createUser);
 
 app.use(auth);
 
